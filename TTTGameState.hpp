a9f8ddff37e2576f0616643bd93b07ac0b12ea4f
kaihvc <47830342+kaihvc@users.noreply.github.com>
2019-04-15 01:54:54 -0600

    Update TTTGameState.hpp

--- TTTGameState.hpp
+++ TTTGameState.hpp
@@ -4,7 +4,18 @@
 #include "GameState.hpp"
 
 class TTTGameState : public GameState {
-  
+private:
+  //Marks which character player and AI are (out of 'x' or 'o')
+  char playerChar;
+  char AIChar;
+public:
+  TTTGameState();
+  ~TTTGameState();
+  void move(std::string move);
+  std::vector<std::string> getValidMoves();
+  bool isValid(std::string move);
+  void print();
+  // int heuristic();
 };
 
 #endif
