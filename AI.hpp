5ccaa1509aaae532c1ef8d7ef1daee1f5380c47b
kaihvc <47830342+kaihvc@users.noreply.github.com>
2019-04-16 18:43:19 -0600

    Update AI.hpp

--- AI.hpp
+++ AI.hpp
@@ -7,15 +7,20 @@ class AI {
 private:
   static const bool HUMAN = false;
   static const bool AI = true;
-  
+
   int maxDepth;
   TreeNode *gameTree;
 
   int minimax(TreeNode *T, int depth, bool player);
 public:
-  AI();
+  AI(GameState gs, int treeDepth){
+
+    //Initialize a gameTree on player's turn
+    gameTree = new TreeNode(gs, treeDepth, true);
+
+  }; //Inline for now
   ~AI();
-  
+
   int getBestMove(GameState gs);
 };
 
