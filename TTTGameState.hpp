f2134d46c08ca917badffb44456c73071c13737a
kaihvc <47830342+kaihvc@users.noreply.github.com>
2019-04-16 18:50:33 -0600

    Update TTTGameState.hpp

--- TTTGameState.hpp
+++ TTTGameState.hpp
@@ -14,7 +14,9 @@ public:
   void move(std::string move);
   std::vector<std::string> getValidMoves();
   bool isValid(std::string move);
+  int isWin(); //0 for no win, 1 for player, 2 for AI
   void print();
+  char* getBoard();
   // int heuristic();
 };
 
