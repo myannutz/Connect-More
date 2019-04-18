b916dd11e2604e8bdffb5440a21b870874f33cf2
Morgan Cluff <43796288+morgancluff@users.noreply.github.com>
2019-04-17 22:27:17 -0600

    Update AI.hpp

--- AI.hpp
+++ AI.hpp
@@ -1,56 +1,25 @@
-#include "AI.hpp"
-#include <vector>
-#include <algorithm>
-#include <climits>
-#include <iostream>
 
-AI::AI(bool player) {
-  maxDepth = 8;
-  gameTree = 0;
-  this->player = player;
-}
+#ifndef AI_HPP
+#define AI_HPP
 
-AI::~AI() {
+#include "TreeNode.hpp"
+#include <string>
 
-}
+class AI {
+private:
+  static const bool HUMAN_PLAYER = false;
+  static const bool AI_PLAYER = true;
 
-std::string AI::getBestMove(GameState *gs) {
-  std::vector<std::string> moves = gs->getValidMoves(player);
-  int bestMoveScore = INT_MIN;
-  std::string bestMove = "";
-  for (auto m : moves) {
-    std::cout << "move: " << m << std::endl;
-    gameTree = new TreeNode(gs->move(m), maxDepth, !player);
-    int score = minimax(gameTree);
-    std::cout << "score: " << score << std::endl;
-    if (score > bestMoveScore) {
-      bestMoveScore = score;
-      bestMove = m;
-    }
-    delete gameTree;
-  }
-  std::cout << "best move h value: " << bestMoveScore << std::endl;
-  return bestMove;
-}
+  int maxDepth;
+  TreeNode *gameTree;
+  bool player; //which player AI is (e.g. white/black, x/o)
 
-int AI::minimax(TreeNode *T) {
-  if (T->getDepth() == 0 || T->getChildren().size() == 0) {
-    //std::cout << "h value: " << T->getGameState()->heuristic(player) << std::endl;
-    return T->getGameState()->heuristic(player); //evaluate favorability for AI
-  }
-  if (T->getPlayer() == !player) {
-    //assume human player will minimize heuristic
-    int val = INT_MAX - 1;
-    for (TreeNode* child : T->getChildren()) {
-      val = std::min(val, minimax(child));
-    }
-    return val;
-  } else {
-    //maximize heuristic
-    int val = INT_MIN + 1;
-    for (TreeNode* child : T->getChildren()) {
-      val = std::max(val, minimax(child));
-    }
-    return val;
-  }
-}
+  int minimax(TreeNode *T);
+public:
+  AI(bool player);
+  ~AI();
+
+  std::string getBestMove(GameState* gs);
+};
+
+#endif
