88721064f7243977624f41241855bfc8f8f5721e
Morgan Cluff <43796288+morgancluff@users.noreply.github.com>
2019-04-17 22:27:01 -0600

    Update AI.hpp

--- AI.hpp
+++ AI.hpp
@@ -1,29 +1,56 @@
-#ifndef AI_HPP
-#define AI_HPP
+#include "AI.hpp"
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <iostream>
 
-#include "TreeNode.hpp"
-#include "GameState.hpp"
-#include <string>
+AI::AI(bool player) {
+  maxDepth = 8;
+  gameTree = 0;
+  this->player = player;
+}
 
-class AI {
-private:
-  static const bool HUMAN_PLAYER = false;
-  static const bool AI_PLAYER = true;
+AI::~AI() {
 
-  int maxDepth;
-  TreeNode *gameTree;
+}
 
-  int minimax(TreeNode *T);
-public:
-  AI(GameState gs, int treeDepth){
+std::string AI::getBestMove(GameState *gs) {
+  std::vector<std::string> moves = gs->getValidMoves(player);
+  int bestMoveScore = INT_MIN;
+  std::string bestMove = "";
+  for (auto m : moves) {
+    std::cout << "move: " << m << std::endl;
+    gameTree = new TreeNode(gs->move(m), maxDepth, !player);
+    int score = minimax(gameTree);
+    std::cout << "score: " << score << std::endl;
+    if (score > bestMoveScore) {
+      bestMoveScore = score;
+      bestMove = m;
+    }
+    delete gameTree;
+  }
+  std::cout << "best move h value: " << bestMoveScore << std::endl;
+  return bestMove;
+}
 
-    //Initialize a gameTree on player's turn
-    gameTree = new TreeNode(gs, treeDepth, true);
-
-  }; //Inline for now
-  ~AI();
-  
-  std::string getBestMove(GameState gs);
-};
-
-#endif
+int AI::minimax(TreeNode *T) {
+  if (T->getDepth() == 0 || T->getChildren().size() == 0) {
+    //std::cout << "h value: " << T->getGameState()->heuristic(player) << std::endl;
+    return T->getGameState()->heuristic(player); //evaluate favorability for AI
+  }
+  if (T->getPlayer() == !player) {
+    //assume human player will minimize heuristic
+    int val = INT_MAX - 1;
+    for (TreeNode* child : T->getChildren()) {
+      val = std::min(val, minimax(child));
+    }
+    return val;
+  } else {
+    //maximize heuristic
+    int val = INT_MIN + 1;
+    for (TreeNode* child : T->getChildren()) {
+      val = std::max(val, minimax(child));
+    }
+    return val;
+  }
+}
