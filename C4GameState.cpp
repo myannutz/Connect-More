437cbf5a42cd579b65fa41ec06f276bafa28af54
Morgan Cluff <43796288+morgancluff@users.noreply.github.com>
2019-04-21 12:07:01 -0600

    Update C4GameState.cpp

--- C4GameState.cpp
+++ C4GameState.cpp
@@ -1,4 +1,5 @@
 #include "C4GameState.hpp"
+#include <iostream>
 
 C4GameState::C4GameState(){
 
@@ -41,9 +42,9 @@ int C4GameState::getUnfilledRow(int column){
 //Pass a string as "cm" - c = column, m = character indicating which player
 GameState C4GameState::move(std::string move){
 
-  int c = (int)move[0];
-  char m = move[2];
-  board[c][getUnfilledRow(c)] = m;
+  int c = move[0] - 48;
+  char m = move[1];
+  board[getUnfilledRow(c)][c] = m;
 
 }
 
@@ -58,7 +59,7 @@ std::vector<std::string> C4GameState::getValidMoves(bool player){
     //If the row isn't full, push back a string
     if(getUnfilledRow(i) != -1){
 
-      moves.push_back(i + playerChar);
+      moves.push_back(std::to_string(i) + std::to_string(playerChar));
 
     }
 
@@ -69,17 +70,12 @@ std::vector<std::string> C4GameState::getValidMoves(bool player){
 }
 
 bool C4GameState::isValid(std::string move, bool player){
-
-  std::vector<std::string> valid = this->getValidMoves(player);
-
-  for(int i = 0; i < valid.size(); i++){
-
-    if(move == valid[i]) return true;
-
-  }
-
-  return false;
-
+  if (move.length() != 2) return false;
+  int c = move[0] - 48;
+  char m = move[1];
+  bool validC = (c > -1 && c < 7 && getUnfilledRow(c) != -1);
+  bool validM = (player == X_PLAYER) ? m == 'x' : m == 'o';
+  return validC && validM;
 }
 
 //Who doesn't love ASCII art
@@ -103,7 +99,7 @@ void C4GameState::print(){
 
 int C4GameState::count2(bool player){
 
-  //Rows
+  //Horizontal
   for(int  i = 0; i < 6; i++){
 
     //Columns
@@ -114,14 +110,19 @@ int C4GameState::count2(bool player){
     }
 
   }
-
+  
+  //vertical
+  
+  //diagonal
 }
 
 int C4GameState::count3(bool player){}
 
-int C4GameState::count4(bool player){}
+int C4GameState::count4(bool player) {}
 
 //High value is good for AI. Low value is good for player.
 int C4GameState::heuristic(bool player){
-
+  int adv = count2(player) + 10*count3(player) + 10000*count4(player);
+  int dis = count2(!player) + 10*count3(!player) + 10000*count4(!player);
+  return adv - dis;
 }
