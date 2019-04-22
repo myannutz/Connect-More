0508cdd32b492b25817329eec556e57ced81483d
Morgan Cluff <43796288+morgancluff@users.noreply.github.com>
2019-04-22 17:38:44 -0600

    Update TreeNode.hpp

--- TreeNode.hpp
+++ TreeNode.hpp
@@ -6,14 +6,19 @@
 
 class TreeNode {
 private:
-  //which player this is (e.g. white/black, x/o)
-  bool player;
-  int depth;
-  GameState *gs;
-  std::vector<TreeNode*> children;
+  bool player; // which player this is (e.g. white/black, x/o)
+  int depth; // the depth of this node in the tree
+  GameState *gs; // the gamestate associated with this node
+  std::vector<TreeNode*> children; // the children of this node
 public:
+  // constructor
+  // @param: gamestate to associate
+  // @param: depth of this node
+  // @param: which player's turn is associated with this node
   TreeNode(GameState *gs, int depth, bool player);
   ~TreeNode();
+  
+  // getters for data members
   std::vector<TreeNode*> getChildren();
   bool getPlayer();
   int getDepth();
