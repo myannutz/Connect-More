97558d12d5263fd49895879fa4686bc740d1b76e
Morgan Cluff <43796288+morgancluff@users.noreply.github.com>
2019-04-22 17:31:27 -0600

    Update GameState.hpp

--- GameState.hpp
+++ GameState.hpp
@@ -13,17 +13,20 @@ public:
   // move function
   // returns a gamestate object created by applying the move
   // @param: string representing move
+  // @return: pointer to a gamestate that has the move applied
   virtual GameState* move(std::string move) = 0;
 
   // getValidMoves function
   // returns a vector of all valid moves for a given player
   // @param: bool representing which player it is
+  // @return: a vector of valid moves represented as strings
   virtual std::vector<std::string> getValidMoves(bool player) = 0;
 
   // isValid function
   // returns true if a move is valid, false if not
   // @param: string representing move
   // @param: bool representing which player it is
+  // @return: whether the given move is valid for the given player
   virtual bool isValid(std::string move, bool player) = 0;
 
   // print function
@@ -33,11 +36,13 @@ public:
   // heuristic function
   // evaluates favorability of the gamestate for a given player
   // @param: bool representing which player it is
+  // @return: how favorable the gamestate is for the given player
   virtual int heuristic(bool player) = 0;
 
   // isWon function
   // returns whether the game is won for a particular player
   // @param: bool representing which player it is
+  // @return: whether the given player has won
   virtual bool isWon(bool player) = 0;
 };
 
