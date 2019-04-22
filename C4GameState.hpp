aa35ad8117400985f31b5112888864d6b271aeff
Morgan Cluff <43796288+morgancluff@users.noreply.github.com>
2019-04-22 17:42:24 -0600

    Update C4GameState.hpp

--- C4GameState.hpp
+++ C4GameState.hpp
@@ -7,15 +7,32 @@
 
 class C4GameState : public GameState{
 private:
-  //X's and O's for connect four? It's more likely than you think
+  // X's and O's for connect four? It's more likely than you think
   static const bool X_PLAYER = false;
   static const bool O_PLAYER = true;
-
+  
+  // getUnfilledRow function
+  // gets the row of the first empty space
+  // @param: column to check
+  // @return: index of row
   int getUnfilledRow(int column);
+  
+  // count2 function
+  // counts how many open 2-in-a-rows there are
+  // @param: bool representing which player to count for
+  // @return: number of open 2-in-a-rows
   int count2(bool player);
+  
+  // count3 function
+  // counts how many open 3-in-a-rows there are
+  // @param: bool representing which player to count for
+  // @return: number of open 3-in-a-rows
   int count3(bool player);
 public:
   C4GameState();
+  
+  // copy constructor
+  // @param: pointer to gamestate to copy
   C4GameState(C4GameState *toCopy);
   ~C4GameState();
 
