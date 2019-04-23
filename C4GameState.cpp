a406c7a8b5d3b119d88dd14ba7004ff08fdde670
Morgan Cluff <43796288+morgancluff@users.noreply.github.com>
2019-04-22 18:06:17 -0600

    Update C4GameState.cpp

--- C4GameState.cpp
+++ C4GameState.cpp
@@ -2,124 +2,108 @@
 #include <iostream>
 
 C4GameState::C4GameState(){
-  board = new char*[6];
+  board = new char*[6]; // allocate memory
   for(int i = 0; i < 6; i++){
-    board[i] = new char[7];
+    board[i] = new char[7]; // allocate memory
     for(int j = 0; j < 7; j++){
-      board[i][j] = ' ';
+      board[i][j] = ' '; // fill board
     }
   }
 }
 
 C4GameState::C4GameState(C4GameState *toCopy) {
-  board = new char*[6];
+  board = new char*[6]; // allocate memory
   for(int i = 0; i < 6; i++){
-    board[i] = new char[7];
+    board[i] = new char[7]; // allocate memory
     for(int j = 0; j < 7; j++){
-      board[i][j] = toCopy->board[i][j];
+      board[i][j] = toCopy->board[i][j]; // copy board
     }
   }
 }
 
 C4GameState::~C4GameState() {
-
   for(int i = 0; i < 6; i++)
-    delete[] board[i];
-
-  delete[] board;
-
+    delete[] board[i]; // free memory
+  delete[] board; // free memory
 }
 
-//Returns the index of the bottom most unfilled space in a particular column (-1 if column is full)
 int C4GameState::getUnfilledRow(int column) {
-  for(int i = 5; i >= 0; i--){
-    if(board[i][column] == ' ') return i;
+  for(int i = 5; i >= 0; i--){ // iterate over rows from bottom to top
+    if(board[i][column] == ' ') return i; // found the open space
   }
   return -1;
 }
 
-//Pass a string as "cm" - c = column, m = character indicating which player
 GameState* C4GameState::move(std::string move) {
-  int c = move[0] - 48;
-  char m = move[1];
-  C4GameState* gs = new C4GameState(this);
-  gs->board[getUnfilledRow(c)][c] = m;
+  int c = move[0] - 48; // what column to move to (shift to convert to int)
+  char m = move[1]; // what piece to place
+  C4GameState* gs = new C4GameState(this); // create new game state
+  gs->board[getUnfilledRow(c)][c] = m; // add piece
   return (GameState*)gs;
 }
 
 std::vector<std::string> C4GameState::getValidMoves(bool player){
-
-  std::vector<std::string> moves;
+  std::vector<std::string> moves; // store valid moves
   char playerChar = 'x';
   if(player == O_PLAYER) playerChar = 'o';
-
-  for(int  i = 0; i < 7; i++){
-
-    //If the row isn't full, push back a string
-    if(getUnfilledRow(i) != -1){
-
-      moves.push_back(std::to_string(i) + playerChar);
-
+  
+  for(int  i = 0; i < 7; i++) {
+    if(getUnfilledRow(i) != -1) { // column is open
+      moves.push_back(std::to_string(i) + playerChar); // add move to vector
     }
-
   }
-
   return moves;
-
 }
 
 bool C4GameState::isValid(std::string move, bool player){
-  if (move.length() != 2) return false;
-  int c = move[0] - 48;
-  char m = move[1];
-  bool validC = (c > -1 && c < 7 && getUnfilledRow(c) != -1);
-  bool validM = (player == X_PLAYER) ? m == 'x' : m == 'o';
+  if (move.length() != 2) return false; // invalid string
+  int c = move[0] - 48; // column
+  char m = move[1]; // piece 
+  bool validC = (c > -1 && c < 7 && getUnfilledRow(c) != -1); // make sure column is within range
+  bool validM = (player == X_PLAYER) ? m == 'x' : m == 'o'; // make sure piece matches player
   return validC && validM;
 }
 
-//Who doesn't love ASCII art
+// who doesn't love ASCII art
 void C4GameState::print(){
-
   std::cout << "=============================" << std::endl;
   for(int i = 0; i < 6; i++){
-
     std::cout << "| ";
     for(int  j = 0; j < 7; j++){
-
       std::cout << board[i][j] << " | ";
-
     }
     std::cout << std::endl;
-
   }
   std::cout << "=============================" << std::endl;
   std::cout << "| 1 | 2 | 3 | 4 | 5 | 6 | 7 |" << std::endl;
-
 }
 
 int C4GameState::count2(bool player){
   char playerChar = 'o';
   if (player == X_PLAYER) playerChar = 'x';
-  int count = 0;
+  int count = 0; // keep track of number
 
-  //horizontal
+  // horizontal
   for (int r = 0; r < 6; r++){
     for (int c = 0; c < 5; c++){
+      // pass a mask over board and check for match
       if (board[r][c] == playerChar && board[r][c + 1] == playerChar && board[r][c + 2] == ' ') count++;
       if (board[r][c] == ' ' && board[r][c + 1] == playerChar && board[r][c + 2] == playerChar) count++;
     }
   }
 
-  //vertical
+  // vertical
   for (int c = 0; c < 7; c++) {
     for (int r = 0; r < 4; r++) {
+      // pass a mask over board and check for match
       if (board[r][c] == ' ' && board[r + 1][c] == playerChar && board[r + 2][c] == playerChar) count++;
     }
   }
 
-  //diagonal
+  // diagonal
   for (int r = 0; r < 4; r++){
     for (int c = 0; c < 5; c++){
+      // pass a mask over board and check for match
       if (board[r][c] == playerChar && board[r + 1][c + 1] == playerChar &&
           board[r + 2][c + 2] == ' ' && (r == 3 || board[r + 3][c + 2] != ' ')) count++;
       if (board[r][c + 2] == playerChar && board[r + 1][c + 1] == playerChar &&
@@ -137,26 +121,29 @@ int C4GameState::count2(bool player){
 int C4GameState::count3(bool player) {
   char playerChar = 'o';
   if (player == X_PLAYER) playerChar = 'x';
-  int count = 0;
+  int count = 0; // keep track of number
 
-  //horizontal
+  // horizontal
   for (int r = 0; r < 6; r++) {
     for (int c = 0; c < 4; c++){
+      // pass a mask over board and check for match
       if (board[r][c] == playerChar && board[r][c + 1] == playerChar && board[r][c + 2] == playerChar && board[r][c + 3] == ' ') count++;
       if (board[r][c] == ' ' && board[r][c + 1] == playerChar && board[r][c + 2] == playerChar && board[r][c + 3] == playerChar) count++;
     }
   }
 
-  //vertical
+  // vertical
   for (int c = 0; c < 7; c++) {
     for (int r = 0; r < 3; r++) {
+      // pass a mask over board and check for match
       if (board[r][c] == ' ' && board[r + 1][c] == playerChar && board[r + 2][c] == playerChar && board[r + 3][c] == playerChar) count++;
     }
   }
 
-  //diagonal
+  // diagonal
   for (int r = 0; r < 3; r++){
     for (int c = 0; c < 4; c++){
+      // pass a mask over board and check for match
       if (board[r][c] == playerChar && board[r + 1][c + 1] == playerChar && board[r + 2][c + 2] == playerChar &&
           board[r + 3][c + 3] == ' ' && (r == 2 || board[r + 4][c + 3] != ' ')) count++;
       if (board[r][c + 3] == playerChar && board[r + 1][c + 2] == playerChar &&
@@ -175,22 +162,28 @@ bool C4GameState::isWon(bool player) {
   char playerChar = 'o';
   if (player == X_PLAYER) playerChar = 'x';
 
+  // horizontal check
   for (int r = 0; r < 6; r++){
     for (int c = 0; c < 4; c++){
+      // pass a mask over board and check for match
       if (board[r][c] == playerChar && board[r][c + 1] == playerChar &&
           board[r][c + 2] == playerChar && board[r][c + 3] == playerChar) return true;
     }
   }
-
+  
+  // vertical check
   for (int c = 0; c < 7; c++) {
     for (int r = 0; r < 3; r++) {
+      // pass a mask over board and check for match
       if (board[r][c] == playerChar && board[r + 1][c] == playerChar &&
           board[r + 2][c] == playerChar && board[r + 3][c] == playerChar) return true;
     }
   }
-
+  
+  // diagonal check
   for (int r = 0; r < 3; r++){
     for (int c = 0; c < 4; c++){
+      // pass a mask over board and check for match
       if (board[r][c] == playerChar && board[r + 1][c + 1] == playerChar &&
           board[r + 2][c + 2] == playerChar && board[r + 3][c + 3] == playerChar) return true;
       if (board[r][c + 3] == playerChar && board[r + 1][c + 2] == playerChar &&
@@ -200,11 +193,11 @@ bool C4GameState::isWon(bool player) {
   return false;
 }
 
-//High value is good for AI. Low value is good for player.
+//high value is good for player, low value is bad for player.
 int C4GameState::heuristic(bool player){
   if (isWon(player)) return 10000;
   if (isWon(!player)) return -10000;
-  int adv = count2(player) + 10*count3(player);
-  int dis = count2(!player) + 10*count3(!player);
+  int adv = count2(player) + 10*count3(player); // how good it is for player
+  int dis = count2(!player) + 10*count3(!player); // how good it is for opponent
   return adv - dis;
 }
